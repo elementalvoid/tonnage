f4d2fe0b725978d2ef30c71715f2f6f02975c578
Matt Klich <mklich@shutterstock.com>
2018-01-10 16:02:26 -0700

    Initial release!

---
+++ README.md
@@ -0,0 +1,118 @@
+# tonnage
+
+>Tonnage is a small utility to summarize the resource capacity and allocation of a Kubernetes cluster. It will inspect pods on a per node basis and then provide both per-node and cluster-wide summary of the resources that are allocatable vs. what has _actually been_ allocated (requests and limits).
+>
+>This inspection can be cluster wide or you can use [label selectors](https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/) to inspect a subset of nodes, pods, or both.
+
+## Table of contents
+  * [Installation](#installation)
+     * [Source install](#source-install)
+     * [Binary install](#binary-install)
+  * [Usage](#usage)
+     * [Targeting clusters](#targeting-clusters)
+     * [Label Selectors](#label-selectors)
+        * [Node Selection](#node-selection)
+        * [Pod Selection](#pod-selection)
+  * [Development](#development)
+    * [Dependencies](#dependencies)
+  * [TODO](#todo)
+  * [Contributing](#contributing)
+  * [License](#license)
+
+## Installation
+
+### Source install
+#### Requirements
+* A working [Go](https://golang.org/) install (tested with 1.8 and 1.9)
+* [dep](https://github.com/golang/dep)
+
+```
+$ go get github.com/elementalvoid/tonnage
+$ cd ${GOPATH}/src/github.com/elementalvoid/tonnage
+$ dep ensure
+$ go install github.com/elementalvoid/tonnage
+```
+
+### Binary install
+See [TODO](#todo)
+
+## Usage
+
+```
+$ tonnage -h 
+```
+
+### Targeting clusters
+Tonnage accepts a `--context` parameter which will cause the client to use the specified "context" from the kubectl configuration file (`kubecfg`).
+```
+$ tonnage --context=minikube
+```
+```
+$ tonnage --context=prod-us-east-1
+```
+
+### Label Selectors
+Label selectors can reduce the inspection to a subset or nodes, pods, or both. This can be handy to summarize tainted nodes, or to see resource consumption from a certain team or set of applications.
+
+From the Kubernetes docs on Labels:
+>Equality- or inequality-based requirements allow filtering by label keys and values. Matching objects must satisfy all of the specified label constraints, though they may have additional labels as well. Three kinds of operators are admitted =,==,!=. The first two represent equality (and are simply synonyms), while the latter represents inequality. For example:
+>```
+>environment = production
+>tier != frontend
+>```
+
+>Set-based label requirements allow filtering keys according to a set of values. Three kinds of operators are supported: in,notin and exists (only the key identifier). For example:
+>```
+>environment in (production, qa)
+>tier notin (frontend, backend)
+>partition
+>!partition
+>```
+
+#### Node selection:
+```
+$ tonnage --node-selector='kubernetes.io/role=node,taint.company.com/app-role=sensitive'
+```
+
+#### Pod selection
+All pods with the label `releaseVersion`.
+```
+$ tonnage --pod-selector='releaseVersion'
+```
+
+
+Only kube-dns and calico-node pods:
+```
+$ tonnage --pod-selector='k8s-app in (kube-dns, calico-node)'
+```
+
+
+## Development
+### Dependencies
+Dependency management uses the [dep](https://github.com/golang/dep) project.
+```
+$ dep ensure
+```
+
+## TODO
+* Automated (binary) builds.
+* Tests.
+* Limit by namespace.
+* Separate scanning from reporting.
+  * Save scans for later.
+  * Save whole scan? Doing so would mean not using built-in k8s selector support if we want to be able to load a scan _and_ further reduce it via selectors.
+* Allow simulating different allocatable resources to answer questions like "What would happen if I used a different AWS/GCE instance type?"
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+1. Fork it (<https://github.com/elementalvoid/tonnage/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+## License
+[Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)
---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"path/filepath"
+
+	flag "github.com/spf13/pflag"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/mitchellh/go-homedir"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type resourceUsage struct {
+	Allocatable struct {
+		CPU    resource.Quantity
+		Memory resource.Quantity
+	}
+	Requests struct {
+		CPU    resource.Quantity
+		Memory resource.Quantity
+	}
+	Limits struct {
+		CPU    resource.Quantity
+		Memory resource.Quantity
+	}
+	NumPods int
+}
+
+var logger = log.New()
+var summaryOnly bool
+var kubeconfig, kubecontext, nodeLabelSelector, podLabelSelector *string
+
+func init() {
+	// handle flags
+	if home, err := homedir.Dir(); home != "" && err == nil {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "Absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "Absolute path to the kubeconfig file")
+	}
+	kubecontext = flag.String("context", "", "The name of the kubeconfig context to use")
+	nodeLabelSelector = flag.String("node-selector", "", "Label selector for nodes to include")
+	podLabelSelector = flag.String("pod-selector", "", "Label selector for pods to include")
+	debug := flag.BoolP("debug", "d", false, "Enable debug logging")
+	colorAlways := flag.BoolP("color-always", "C", false, "Force color output in all cases")
+	colorNever := flag.Bool("color-never", false, "Disable color completely")
+	json := flag.Bool("json", false, "Enable JSON formatted logging")
+	flag.BoolVarP(&summaryOnly, "summary", "s", false, "Show only the cummulative cluster summary")
+	flag.Parse()
+
+	if *json {
+		logger.Formatter = &log.JSONFormatter{}
+	} else {
+		logger.Formatter = &log.TextFormatter{
+			ForceColors:   *colorAlways,
+			DisableColors: *colorNever,
+			FullTimestamp: true,
+		}
+	}
+
+	if *debug {
+		logger.SetLevel(log.DebugLevel)
+	}
+}
+
+func main() {
+	client, err := getClient()
+	if err != nil {
+		logger.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Fatal("Error creating kube client.")
+	}
+
+	nodeList, err := client.CoreV1().Nodes().List(metav1.ListOptions{
+		LabelSelector: *nodeLabelSelector,
+	})
+	if err != nil {
+		logger.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Fatal("Error listing nodes.")
+	}
+	logger.WithFields(log.Fields{
+		"numNodes":          len(nodeList.Items),
+		"nodeLabelselector": *nodeLabelSelector,
+	}).Info("Found nodes")
+
+	clusterResources := resourceUsage{}
+
+	for _, n := range nodeList.Items {
+		node := resourceUsage{}
+
+		node.Allocatable.CPU = *n.Status.Allocatable.Cpu()
+		node.Allocatable.Memory = *n.Status.Allocatable.Memory()
+
+		nodeFieldSelector := fields.OneTermEqualSelector("spec.nodeName", n.GetName()).String()
+		podsList, err := client.CoreV1().Pods("").List(metav1.ListOptions{
+			// Apply label selector from cli args
+			LabelSelector: *podLabelSelector,
+			// Limit to Pods on the current node
+			FieldSelector: nodeFieldSelector,
+		})
+		if err != nil {
+			logger.WithFields(log.Fields{
+				"node":  n.Name,
+				"error": err.Error(),
+			}).Fatal("Error listing pods for node.")
+		}
+		node.NumPods = len(podsList.Items)
+		for _, p := range podsList.Items {
+			for _, c := range p.Spec.Containers {
+				reqCPU := c.Resources.Requests["cpu"]
+				reqMemory := c.Resources.Requests["memory"]
+				limCPU := c.Resources.Limits["cpu"]
+				limMemory := c.Resources.Limits["memory"]
+				logger.WithFields(log.Fields{
+					"pod":       p.Name,
+					"container": c.Name,
+					"requests": log.Fields{
+						"cpu":    reqCPU.String(),
+						"memory": reqMemory.String(),
+					},
+					"limits": log.Fields{
+						"cpu":    limCPU.String(),
+						"memory": limMemory.String(),
+					},
+				}).Debug("Per container usage")
+				node.Requests.CPU.Add(reqCPU)
+				node.Requests.Memory.Add(reqMemory)
+				node.Limits.CPU.Add(limCPU)
+				node.Limits.Memory.Add(limMemory)
+			}
+		}
+
+		clusterResources.accumulate(node)
+
+		if !summaryOnly {
+			logger.WithFields(log.Fields{
+				"node":    n.Name,
+				"numPods": node.NumPods,
+				"allocatable": log.Fields{
+					"cpu":    node.Allocatable.CPU.String(),
+					"memory": node.Allocatable.Memory.String(),
+				},
+				"requests": log.Fields{
+					"cpu":    node.Requests.CPU.String(),
+					"memory": node.Requests.Memory.String(),
+				},
+				"limits": log.Fields{
+					"cpu":    node.Limits.CPU.String(),
+					"memory": node.Limits.Memory.String(),
+				},
+			}).Info("Per node usage.")
+		}
+	}
+
+	logger.WithFields(log.Fields{
+		"numPods": clusterResources.NumPods,
+		"allocatable": log.Fields{
+			"cpu":    clusterResources.Allocatable.CPU.String(),
+			"memory": clusterResources.Allocatable.Memory.String(),
+		},
+		"requests": log.Fields{
+			"cpu":    clusterResources.Requests.CPU.String(),
+			"memory": clusterResources.Requests.Memory.String(),
+		},
+		"limits": log.Fields{
+			"cpu":    clusterResources.Limits.CPU.String(),
+			"memory": clusterResources.Limits.Memory.String(),
+		},
+	}).Info("Cummulative cluster usage.")
+}
+
+func (r *resourceUsage) accumulate(node resourceUsage) {
+	r.NumPods += node.NumPods
+	r.Allocatable.CPU.Add(node.Allocatable.CPU)
+	r.Allocatable.Memory.Add(node.Allocatable.Memory)
+	r.Requests.CPU.Add(node.Requests.CPU)
+	r.Requests.Memory.Add(node.Requests.Memory)
+	r.Limits.CPU.Add(node.Limits.CPU)
+	r.Limits.Memory.Add(node.Limits.Memory)
+}
+
+func getClient() (*kubernetes.Clientset, error) {
+	// create a client config with proper kubeconfig and context
+	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: *kubeconfig},
+		&clientcmd.ConfigOverrides{CurrentContext: *kubecontext}).ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	serverInfo, err := clientset.Discovery().ServerVersion()
+	if err != nil {
+		logger.WithFields(log.Fields{
+			"server": "unknown",
+			"err":    err.Error(),
+		}).Info("Connected to Kuberenetes API - unable to determine version")
+	} else {
+		logger.WithFields(log.Fields{
+			"server": serverInfo.String(),
+		}).Info("Connected to Kuberenetes API")
+	}
+	return clientset, nil
+}
